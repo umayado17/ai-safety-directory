2b0552ef16cf938efdb23496a260c0d0ececee87
umayado17 <hiraoka@da2.so-net.ne.jp>
2026-03-25 00:00:16 +0900

    こわれた記述を修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { supabase } from '@/lib/supabase_typo'
+import { supabase } from '@/lib/supabase'
 
 type Service = {
   id: string
