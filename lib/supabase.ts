f9eb8d2a59a9f91d1307fc6ec7277cf0384642f1
umayado17 <hiraoka@da2.so-net.ne.jp>
2026-03-25 00:33:20 +0900

    supabase.tsにコメント追加

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-
+// Supabase接続設定
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
